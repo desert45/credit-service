dd7d12c65659da2d2f9d787ff959626125b04dd4
Antony Fernandez <62619196+desert45@users.noreply.github.com>
2022-07-21 00:42:46 -0500

    commit -m

--- src/main/java/com/bootcamp/credit/CreditServiceApplication.java
+++ src/main/java/com/bootcamp/credit/CreditServiceApplication.java
@@ -1,7 +1,11 @@
 package com.bootcamp.credit;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class CreditServiceApplication {
@@ -10,4 +14,18 @@ public class CreditServiceApplication {
 		SpringApplication.run(CreditServiceApplication.class, args);
 	}
 
+}
+
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+    @Value("${service.name}")
+    private String name;
+
+    @RequestMapping("/name")
+    String getMessage() {
+        return this.name;
+    }
 }
