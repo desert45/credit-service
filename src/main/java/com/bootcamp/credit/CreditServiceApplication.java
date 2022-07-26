7193a6ccbf516d75cd06e8da976a59f56c218f47
Antony Fernandez <62619196+desert45@users.noreply.github.com>
2022-07-26 16:26:41 -0500

    commit afc

--- src/main/java/com/bootcamp/credit/CreditServiceApplication.java
+++ src/main/java/com/bootcamp/credit/CreditServiceApplication.java
@@ -1,12 +1,10 @@
 package com.bootcamp.credit;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class CreditServiceApplication {
 
@@ -14,18 +12,4 @@ public class CreditServiceApplication {
 		SpringApplication.run(CreditServiceApplication.class, args);
 	}
 
-}
-
-
-@RefreshScope
-@RestController
-class MessageRestController {
-
-    @Value("${service.name}")
-    private String name;
-
-    @RequestMapping("/name")
-    String getMessage() {
-        return this.name;
-    }
 }
